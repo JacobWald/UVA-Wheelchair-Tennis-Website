c45dae1923fecef196956db6718ec304d2865fe0
Jacob Wald <vug5ue@virginia.edu>
2023-08-06 20:32:28 -0400

    navbar updated to contain svg of sabers

--- tennis-website/src/components/NavBar/NavBar.js
+++ tennis-website/src/components/NavBar/NavBar.js
@@ -7,28 +7,31 @@ import HomeIcon from '@mui/icons-material/Home';
 export default function NavBar() {
     return (
         <div className='navbar'>
-            <div className="navbar-content">
-                <Link to="/">
-                    <Button>
-                        <HomeIcon sx={{ fontSize: '45px' }} />
-                    </Button>
-                </Link>
-                <Link to="/about">
-                    <Button>
-                        About
-                    </Button>
-                </Link>
-                <Link to="/meetTheTeam">
-                    <Button>
-                        Meet The Team
-                    </Button>
-                </Link>
-                <Link to="/accomplishments">
-                    <Button>
-                        Accomplishments
-                    </Button>
-                </Link>
-            </div>
+                <div className="navbar-left">
+                    <Link to="/">
+                        <Button>
+                            <HomeIcon sx={{ fontSize: '45px' }} />
+                        </Button>
+                    </Link>
+                    <Link to="/about">
+                        <Button>
+                            About
+                        </Button>
+                    </Link>
+                    <Link to="/meetTheTeam">
+                        <Button>
+                            Meet The Team
+                        </Button>
+                    </Link>
+                    <Link to="/accomplishments">
+                        <Button>
+                            Accomplishments
+                        </Button>
+                    </Link>
+                </div>
+                <div className="navbar-right">
+                    <img src="./images/sabers.svg" alt="UVA Sabers SVG" className="sabers-logo"/>
+                </div>
         </div>
     );
 }
