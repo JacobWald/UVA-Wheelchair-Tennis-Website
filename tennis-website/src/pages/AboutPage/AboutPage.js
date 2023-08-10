6296bf457887b9fe131deabe697cea095cc6a1e3
Jacob Wald <vug5ue@virginia.edu>
2023-08-09 22:21:52 -0400

    support links added. merged with about page

--- tennis-website/src/pages/AboutPage/AboutPage.js
+++ tennis-website/src/pages/AboutPage/AboutPage.js
@@ -7,6 +7,20 @@ export default function AboutPage() {
             <div className='about-page'>
                 <h1>About</h1>
             </div>
+            <div className="links">
+                <a href="https://www.instagram.com/uva_wheelchair_tennis/" class="button-link">
+                    <img src="./images/instagram.svg" alt="Instagram SVG" className="icon"/>
+                    Instagram
+                </a>
+                <a href="https://www.gofundme.com/f/the-wheelchair-tennis-team-at-uva?utm_medium=referral&utm_source=widget&utm_campaign=p_cp%2Bshare-sheet" class="button-link">
+                    {/* <img src="./images/sabers.svg" alt="GoFundMe SVG" className="icon"/> */}
+                    GoFundMe
+                </a>
+                <a href="mailto:vug5ue@virginia.edu" class="button-link">
+                    <img src="./images/mail.svg" alt="Mail SVG" className="icon"/>
+                    Contact Us
+                </a>
+            </div>
             <div className='our-story'>
                 <div className='left-text'>
                     <h1>Our Story</h1>
@@ -23,27 +37,6 @@ export default function AboutPage() {
                 <div className='right-text'>
                     <h1>The Need</h1>
                     <p className="info">{text.theNeed}</p>
-                </div>
-            </div>
-            <div className='support'>
-                <div className= 'support-title'>
-                    <h1>Support The Team</h1>
-                </div>
-                <div className="links">
-                    <div>
-                        <a href="#" class="button-link">
-                            <img src="./images/instagram.svg" alt="Instagram SVG" className="icon"/>
-                            Instagram
-                        </a>
-                        <a href="#" class="button-link">
-                            {/* <img src="./images/sabers.svg" alt="GoFundMe SVG" className="icon"/> */}
-                            GoFundMe
-                        </a>
-                        <a href="#" class="button-link">
-                            <img src="./images/mail.svg" alt="Mail SVG" className="icon"/>
-                            Contact Us
-                        </a>
-                    </div>
                 </div>
             </div>
         </>
