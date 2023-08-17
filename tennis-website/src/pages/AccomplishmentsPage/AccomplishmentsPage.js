e3f1a41fba17c83b2a57bbee87d501c4b68d9548
Jacob Wald <vug5ue@virginia.edu>
2023-08-17 15:35:55 -0400

    Accomplishment descriptions added

--- tennis-website/src/pages/AccomplishmentsPage/AccomplishmentsPage.js
+++ tennis-website/src/pages/AccomplishmentsPage/AccomplishmentsPage.js
@@ -1,4 +1,5 @@
 import "./AccomplishmentsPage.css";
+import { accomplishments } from '../../teamInfo/accomplishments';
 
 export default function AccomplishmentsPage() {
     return (
@@ -6,29 +7,22 @@ export default function AccomplishmentsPage() {
             <div className='accomplishments-page'>
                 <h1>Accomplishments Page</h1>
             </div>
-            <div class="accomplishment">
-                <img src="images/accomplishmentsPage/allAmerican.jpg" alt="All American" />
-                <div class="accomplishment-details">
-                    <h2 class="accomplishment-title">All American</h2>
-                    <p class="accomplishment-description">Description of the All American accomplishment.</p>
-                </div>
-            </div>
-    
-            <div class="accomplishment">
-                <img src="images/accomplishmentsPage/nationalTitle.jpg" alt="National Title" />
-                <div class="accomplishment-details">
-                    <h2 class="accomplishment-title">National Title</h2>
-                    <p class="accomplishment-description">Description of the National Title accomplishment.</p>
-                </div>
-            </div>
-    
-            <div class="accomplishment">
-                <img src="images/accomplishmentsPage/CDoublesFirst.jpg" alt="Rivercity Slam Doubles Victory" />
-                <div class="accomplishment-details">
-                    <h2 class="accomplishment-title">Rivercity Slam Doubles Victory</h2>
-                    <p class="accomplishment-description">Description of the C Doubles First Place accomplishment.</p>
-                </div>
-            </div>
+                {accomplishments.map((accomplishment) => 
+                    <div class="accomplishment">
+                        <img src={accomplishment.image} alt="All American" />
+                        <div class="accomplishment-details">
+                            <h2 class="accomplishment-title">{accomplishment.name}</h2>
+                            <p class="accomplishment-description">{accomplishment.description}</p>
+                            {accomplishment.article ? 
+                            <a href={accomplishment.article} className="article">
+                                Read the article
+                            </a>
+                            :
+                            null
+                            }
+                        </div>
+                    </div>
+                )}
         </>
     );
 }
