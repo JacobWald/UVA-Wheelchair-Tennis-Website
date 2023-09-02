874ae15f0bc6d5e08f142d73870476eaf938332a
Jacob Wald <vug5ue@virginia.edu>
2023-09-02 17:26:12 -0400

    email added

--- tennis-website/src/components/Footer/Footer.js
+++ tennis-website/src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
         <div className="footer">
             <img src="./images/sabers.svg" alt="UVA Sabers SVG" className="sabers-logo"/>
             <div>
-                <p>email@email.com</p>
+                <p>vug5ue@virginia.edu</p>
                 <p>© 2023 UVA Adaptive Athletics</p>
             </div>
         </div>
